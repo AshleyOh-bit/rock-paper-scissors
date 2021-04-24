74fcd4031aeb699645aa69e60bf3e82f9ededbc7
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-04-24 15:59:37 -0600

    Add difficultGameCheck method

--- game.js
+++ game.js
@@ -50,4 +50,53 @@ class Game {
       resetGame() {
       // localStorage.clear();
     }
+
+    //rock beats scissors and computer
+//paper beats rock and computer
+//scissors beats paper and alien
+//alien beats rock and paper
+//computer beats alien and scissors
+
+
+  difficultGameCheck() {
+    //check for a tie first
+      if (this.humanPlayer.token === this.compPlayer.token) {
+        return this.winner = false;
+    //check if the human player wins
+      } else if (this.humanPlayer.token === "rock" &&   ((this.compPlayer.token === "scissors") || (this.compPlayer.token === "computer"))) {
+        this.humanPlayer.wins++;
+        return this.winner = this.humanPlayer.name;
+      } else if (this.humanPlayer.token === "paper" &&   ((this.compPlayer.token === "rock") || (this.compPlayer.token === "computer"))) {
+        this.humanPlayer.wins++;
+        return this.winner = this.humanPlayer.name;
+        } else if (this.humanPlayer.token === "scissors" &&   ((this.compPlayer.token === "paper") || (this.compPlayer.token === "alien"))) {
+        this.humanPlayer.wins++;
+        return this.winner = this.humanPlayer.name;
+        } else if (this.humanPlayer.token === "alien" &&   ((this.compPlayer.token === "paper") || (this.compPlayer.token === "rock"))) {
+        this.humanPlayer.wins++;
+        return this.winner = this.humanPlayer.name;
+        } else if (this.humanPlayer.token === "computer" &&   ((this.compPlayer.token === "alien") || (this.compPlayer.token === "scissors"))) {
+        this.humanPlayer.wins++;
+        return this.winner = this.humanPlayer.name;
+        //check if computer wins
+        } else if (this.compPlayer.token === "rock" && (this.humanPlayer.token === "scissors") || (this.humanPlayer.token === "computer")) {
+          this.compPlayer.wins++;
+          return this.winner = this.compPlayer.name;
+        } else if (this.compPlayer.token === "paper" && (this.humanPlayer.token === "rock") || (this.humanPlayer.token === "computer")) {
+          this.compPlayer.wins++;
+          return this.winner = this.compPlayer.name;
+        } else if (this.compPlayer.token === "scissors" && (this.humanPlayer.token === "paper") || (this.humanPlayer.token === "alien")) {
+          this.compPlayer.wins++;
+          return this.winner = this.compPlayer.name;
+        } else if (this.compPlayer.token === "alien" && (this.humanPlayer.token === "rock") || (this.humanPlayer.token === "paper")) {
+          this.compPlayer.wins++;
+          return this.winner = this.compPlayer.name;
+        } else if (this.compPlayer.token === "computer" && (this.humanPlayer.token === "alien") || (this.humanPlayer.token === "scissors")) {
+          this.compPlayer.wins++;
+          return this.winner = this.compPlayer.name;
+          //remove this console.log if it works
+        } else {
+          console.log("something went wrong");
+        }
+      }
 }
