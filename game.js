4057f21492334bc335141146dd371a745b7531af
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-04-24 16:04:20 -0600

    Add console.logs to test player.js and game.js

--- game.js
+++ game.js
@@ -11,6 +11,10 @@ class Game {
    }
  }
 
+ resetGame() {
+ // localStorage.clear();
+  }
+
  determineCompChoice() {
      var compNum;
      if (this.gameType === "classic") {
@@ -47,9 +51,6 @@ class Game {
         }
       }
 
-      resetGame() {
-      // localStorage.clear();
-    }
 
     //rock beats scissors and computer
     //paper beats rock and computer
@@ -58,45 +59,77 @@ class Game {
     //computer beats alien and scissors
 
 
-  difficultGameCheck() {
-    //check for a tie first
-      if (this.humanPlayer.token === this.compPlayer.token) {
-        return this.winner = false;
-    //check if the human player wins
-      } else if (this.humanPlayer.token === "rock" &&   ((this.compPlayer.token === "scissors") || (this.compPlayer.token === "computer"))) {
-        this.humanPlayer.wins++;
-        return this.winner = this.humanPlayer.name;
-      } else if (this.humanPlayer.token === "paper" &&   ((this.compPlayer.token === "rock") || (this.compPlayer.token === "computer"))) {
-        this.humanPlayer.wins++;
-        return this.winner = this.humanPlayer.name;
-        } else if (this.humanPlayer.token === "scissors" &&   ((this.compPlayer.token === "paper") || (this.compPlayer.token === "alien"))) {
-        this.humanPlayer.wins++;
-        return this.winner = this.humanPlayer.name;
-        } else if (this.humanPlayer.token === "alien" &&   ((this.compPlayer.token === "paper") || (this.compPlayer.token === "rock"))) {
-        this.humanPlayer.wins++;
-        return this.winner = this.humanPlayer.name;
-        } else if (this.humanPlayer.token === "computer" &&   ((this.compPlayer.token === "alien") || (this.compPlayer.token === "scissors"))) {
-        this.humanPlayer.wins++;
-        return this.winner = this.humanPlayer.name;
-        //check if computer wins
-        } else if (this.compPlayer.token === "rock" && (this.humanPlayer.token === "scissors") || (this.humanPlayer.token === "computer")) {
-          this.compPlayer.wins++;
-          return this.winner = this.compPlayer.name;
-        } else if (this.compPlayer.token === "paper" && (this.humanPlayer.token === "rock") || (this.humanPlayer.token === "computer")) {
-          this.compPlayer.wins++;
-          return this.winner = this.compPlayer.name;
-        } else if (this.compPlayer.token === "scissors" && (this.humanPlayer.token === "paper") || (this.humanPlayer.token === "alien")) {
-          this.compPlayer.wins++;
-          return this.winner = this.compPlayer.name;
-        } else if (this.compPlayer.token === "alien" && (this.humanPlayer.token === "rock") || (this.humanPlayer.token === "paper")) {
-          this.compPlayer.wins++;
-          return this.winner = this.compPlayer.name;
-        } else if (this.compPlayer.token === "computer" && (this.humanPlayer.token === "alien") || (this.humanPlayer.token === "scissors")) {
-          this.compPlayer.wins++;
-          return this.winner = this.compPlayer.name;
-          //remove this console.log if it works
-        } else {
-          console.log("something went wrong");
+    difficultGameCheck() {
+      //check for a tie first
+        if (this.humanPlayer.token === this.compPlayer.token) {
+          return this.winner = false;
+      //check if the human player wins
+        } else if (this.humanPlayer.token === "rock" &&   ((this.compPlayer.token === "scissors") || (this.compPlayer.token === "computer"))) {
+          this.humanPlayer.wins++;
+          return this.winner = this.humanPlayer.name;
+        } else if (this.humanPlayer.token === "paper" &&   ((this.compPlayer.token === "rock") || (this.compPlayer.token === "computer"))) {
+          this.humanPlayer.wins++;
+          return this.winner = this.humanPlayer.name;
+          } else if (this.humanPlayer.token === "scissors" &&   ((this.compPlayer.token === "paper") || (this.compPlayer.token === "alien"))) {
+          this.humanPlayer.wins++;
+          return this.winner = this.humanPlayer.name;
+          } else if (this.humanPlayer.token === "alien" &&   ((this.compPlayer.token === "paper") || (this.compPlayer.token === "rock"))) {
+          this.humanPlayer.wins++;
+          return this.winner = this.humanPlayer.name;
+          } else if (this.humanPlayer.token === "computer" &&   ((this.compPlayer.token === "alien") || (this.compPlayer.token === "scissors"))) {
+          this.humanPlayer.wins++;
+          return this.winner = this.humanPlayer.name;
+          //check if computer wins
+          } else if (this.compPlayer.token === "rock" && (this.humanPlayer.token === "scissors") || (this.humanPlayer.token === "computer")) {
+            this.compPlayer.wins++;
+            return this.winner = this.compPlayer.name;
+          } else if (this.compPlayer.token === "paper" && (this.humanPlayer.token === "rock") || (this.humanPlayer.token === "computer")) {
+            this.compPlayer.wins++;
+            return this.winner = this.compPlayer.name;
+          } else if (this.compPlayer.token === "scissors" && (this.humanPlayer.token === "paper") || (this.humanPlayer.token === "alien")) {
+            this.compPlayer.wins++;
+            return this.winner = this.compPlayer.name;
+          } else if (this.compPlayer.token === "alien" && (this.humanPlayer.token === "rock") || (this.humanPlayer.token === "paper")) {
+            this.compPlayer.wins++;
+            return this.winner = this.compPlayer.name;
+          } else if (this.compPlayer.token === "computer" && (this.humanPlayer.token === "alien") || (this.humanPlayer.token === "scissors")) {
+            this.compPlayer.wins++;
+            return this.winner = this.compPlayer.name;
+            //remove this console.log if it works
+          } else {
+            console.log("something went wrong");
+          }
         }
-      }
-}
+  }
+
+//REMOVE THESE CONSOLE LOGS BEFORE DEPLOYMENT:
+//TESTS:
+  // var alienPlayer = new Player("susie", "rock", 0);
+  // console.log(alienPlayer.token);
+
+  // var compPlayer = new Player("sheborg", "", 0);
+  // console.log(compPlayer.token);
+
+  // console.log(alienPlayer.takeTurn("classic"));
+  // console.log(compPlayer.takeTurn("classic"));
+
+  // var firstGame = new Game(alienPlayer, compPlayer, "classic");
+
+  // console.log(firstGame.determineCompChoice());
+
+  // console.log(firstGame.checkForWinner());
+
+  // console.log(firstGame.winner);
+  // console.log(firstGame.compPlayer.token);
+
+  // console.log(compPlayer.wins);
+  // console.log(alienPlayer.wins);
+
+  // var ashley = new Player("ash", "computer", 0);
+  // var compy = new Player("gertrund.0", "computer", 0);
+  //
+  // var secondGame = new Game(ashley, compy, "difficult");
+  //
+  // secondGame.difficultGameCheck();
+  //
+  // console.log(secondGame.winner);
