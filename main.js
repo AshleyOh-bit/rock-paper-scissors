b6e68819cd681d4f77b7fd6e2f245efdd9728dd9
Ashley O'Brien <ashley.s.obrien@gmail.com>
2021-04-25 13:20:38 -0600

    Implement changeGameButton functionality

--- main.js
+++ main.js
@@ -2,9 +2,11 @@
 var human, computer, game;
 
 // Query Selectors
+////Game Views
 var gameChoice = document.querySelector("#gameChoice");
 var classicFighters = document.querySelector("#classicFighters");
 var difficultFighters = document.querySelector("#difficultFighters");
+var gamePlayView = document.querySelector("#gamePlay");
 
 //// Icons
 var rockIconClassic = document.querySelector("#rockIconClassic");
@@ -17,22 +19,36 @@ var scissorsIconDiff = document.querySelector("#scissorsIconDiff");
 var alienIconDiff = document.querySelector("#alienIconDiff");
 var computerIconDiff = document.querySelector("#computerIconDiff");
 
-var gamePlayView = document.querySelector("#gamePlay");
+// Button
+var changeGameButton = document.querySelector("#changeGame");
+
 
 
 
 // Event listeners
 gameChoice.addEventListener("click", setGameType);
-classicFighters.addEventListener("click", reserveFighterChoice)
+classicFighters.addEventListener("click", reserveFighterChoice);
+changeGameButton.addEventListener("click", returnHome)
 
 // Event Handlers
 function hide(element) {
-  element.classList.add("hidden");
+  if (element === changeGameButton) {
+    element.classList.add("invisible")
+  } else {
+    element.classList.add("hidden");
+  }
 }
 
 function display(element) {
   element.classList.remove("hidden");
+  element.classList.remove("invisible");
 }
+
+
+
+// function reveal(element) {
+//   element.classList.remove("invisible");
+// }
 
 function renderGamePlay(player1, player2, view) {
   hide(view);
@@ -45,6 +61,7 @@ function renderGamePlay(player1, player2, view) {
   setTimeout(function() {
     display(view)
     hide(gamePlayView)
+    display(changeGameButton)
   }, 2000);
 }
 
@@ -71,8 +88,11 @@ function setGameType() {
 function reserveFighterChoice() {
   //updates the humanPlayer class + game class gametype
   game.determineCompChoice();
+  console.log(game.compPlayer.token)
+  console.log(computer.token)
   if (event.target.closest("#rockIconClassic")) {
     human.token = game.gameData.classic[0];
+    computer.token = game.compPlayer.token;
     console.log(computer.token)
     //game.determineCompChoice();
     // hide(classicFighters);
@@ -80,12 +100,14 @@ function reserveFighterChoice() {
     renderGamePlay(human, computer, classicFighters);
   } else if (event.target.closest("#paperIconClassic")) {
     human.token = game.gameData.classic[1];
+    computer.token = game.compPlayer.token;
     //game.determineCompChoice();
     //hide(classicFighters);
     //display(gamePlayView);
     renderGamePlay(human, computer, classicFighters);
   } else if (event.target.closest("#scissorsIconClassic")) {
     human.token = game.gameData.classic[2];
+    computer.token = game.compPlayer.token;
     //game.determineCompChoice();
     //hide(classicFighters);
     //display(gamePlayView);
@@ -98,4 +120,11 @@ function reserveFighterChoice() {
   //displays game view
   //renders game view
   //calls timeout function
+}
+
+function returnHome() {
+  hide(classicFighters);
+  hide(difficultFighters);
+  display(gameChoice);
+  hide(changeGameButton);
 }
